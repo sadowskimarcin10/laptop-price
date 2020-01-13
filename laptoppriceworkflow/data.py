2764f582eb50615c2e4a094d5b33fde7627b2339
sadowskimarcin10 <52209211+sadowskimarcin10@users.noreply.github.com>
2020-01-13 23:44:46 +0100

    Fix bugs

--- laptoppriceworkflow/data.py
+++ laptoppriceworkflow/data.py
@@ -1,13 +1,15 @@
-def get_data():
+def get_json_data(filname = 'public-dataset.json'):
     """Download and cache the laptop-dataset
     Parameters
     ====
+    filename : string
+        Name of data
     Returns
     ====
     data : pandas.DataFrame
         The laptop price data
     """
     import pandas as pd
-    dataset = pd.read_json("laptoppriceworkflow/public-dataset.json")
+    dataset = pd.read_json(filname)
     df = pd.DataFrame(dataset)
-    return df
+    return dataset
